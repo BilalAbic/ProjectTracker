b299726a6d416e9988f4a7859d3af7703ff794bb
Bilal Abiç <bilalabic78@gmail.com>
2026-01-06 12:19:40 +0300

    Fix repository name case in config.js

--- web/js/config.js
+++ web/js/config.js
@@ -8,7 +8,7 @@ const CONFIG = {
     API_BASE_URL: 'https://bilalabic.com',
     
     // Uygulama indirme linki
-    DOWNLOAD_URL: 'https://github.com/BilalAbic/projecttracker/releases/latest',
+    DOWNLOAD_URL: 'https://github.com/BilalAbic/ProjectTracker/releases/latest',
     
     // Demo modu - false yaparak production'da kullanın
     DEMO_MODE: false
